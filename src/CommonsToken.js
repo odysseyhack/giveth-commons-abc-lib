e0374028e00a813f3996a323d8ce776a6ff52e4d
dOrgJelli <jelli@dorg.tech>
2019-04-13 17:16:07 +0200

    construction refinement

--- src/CommonsToken.js
+++ src/CommonsToken.js
@@ -5,17 +5,53 @@ var abi = require("@giveth/commons-abc-contracts/build/contracts/CommonsToken.js
 
 class CommonsToken
 {
-  constructor(
+  constructor(contract) {
+    this.contract = contract;
+  }
+
+  static fromAddress(
     address
   ) {
-    let web3 = Web3.getWeb3();
+    const web3 = Web3.getWeb3();
 
     assert(web3);
     assert(web3.utils.isAddress(address));
 
-    this.contract = web3.eth.Contract(
+    const contract = web3.eth.Contract(
       abi.abi, address, utils.getWeb3Options(web3)
     );
+
+    return new CommonsToken(contract);
+  }
+
+  static fromParams(
+    reserveToken,
+    reserveRatio,
+    gasPrice,
+    theta,
+    p0,
+    initialRaise,
+    fundingPool,
+    friction
+  ) {
+    const web3 = Web3.getWeb3();
+    const toDeploy = web3.eth.contract(abi.abi);
+
+    const contract = toDeploy.new(
+      reserveToken,
+      reserveRatio,
+      gasPrice,
+      theta,
+      p0,
+      initialRaise,
+      fundingPool,
+      friction, {
+        from: web3.defaultAccount,
+        data: abi.bytecode
+      }
+    );
+
+    return new CommonsToken(contract);
   }
 
   mint(amount) {
--- test/commonsToken.spec.js
+++ test/commonsToken.spec.js
@@ -21,4 +21,61 @@ describe("CommonToken", () => {
     const contract = new ABC.CommonsToken(address);
     expect(contract).to.not.be.null;
   });
+
+  it("constructor(...)", () => {
+    const contract = new ABC.CommonsToken(
+      "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+      142857,
+      15000000000,
+      350000,
+      1,
+      300000,
+      20000,
+      15000000000
+    );
+    expect(contract).to.not.be.null;
+  });
+
+  it("mint", () => {
+
+  });
 });
+
+// theta
+// calculatePurchaseReturn
+// increaseAllowance
+// calculateSaleReturn
+// version
+// calculateCurvedBurnReturn
+// balanceOf
+// poolBalance
+// calculateCurvedMintReturn
+// friction
+// decreaseAllowance
+// transfer
+// allowance
+// initialRaise
+// raised
+// reserveToken
+// fundingPool
+// gasPrice
+// CurvedMint
+// CurvedBurn
+// mint
+// burn
+// hatchContribute
+// fundsAllocated
+// claimTokens
+
+/// HIGH-PRI
+/// VIEWS
+// reserveRatio view (should be 142857)
+// totalSupply view (should be how mahy tokens are minted)
+// isInHatchingPhase view (should be true)
+// p0 view (should be 1)
+
+/// MISC
+// approve(address spender, uint value) => (true/false)
+// Approval event(sender, spender, value)
+// transferFrom(address from, address to, uint value)
+// Approval event(from, msg.sender, value)
